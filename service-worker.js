0dda58ca34fb6b76ca96f85dca8ca069fd9b356d
tavilu <tarcisiobruno02@hotmail.com>
2025-11-21 15:01:12 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', function(event) { self.skipWaiting(); });
+self.addEventListener('activate', function(event) { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', function(event) {});
