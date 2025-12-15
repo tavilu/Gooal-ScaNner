f01948ac725ac1d97a2f5babc6aeeb19c5e78527
tavilu <tarcisiobruno02@hotmail.com>
2025-12-15 16:28:39 -0300

    telegram_service.py

--- services/telegram_service.py
+++ services/telegram_service.py
@@ -5,9 +5,12 @@ BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(message: str):
+    print("DEBUG Telegram:", BOT_TOKEN, CHAT_ID)
+
     if not BOT_TOKEN or not CHAT_ID:
         print("⚠️ Telegram não configurado")
         return
+
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
